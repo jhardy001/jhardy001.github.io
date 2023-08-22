5d1001767543fe4dbcef77dc02ef43d34c273e7a
jhardy001 <jhardy001@converse.edu>
2023-08-22 13:48:59 -0400

    Final commit

--- CSC 235/Homework8/hw8-work.js
+++ CSC 235/Homework8/hw8-work.js
@@ -13,21 +13,20 @@ function validForm(formElt) {
 function somethingChecked(parentID) {
     let parentElt = document.getElementById(parentID);
     let boxes = parentElt.querySelectorAll('input[type=checkbox]');
-    let flag = parentElt.querySelector('.flag');
-    console.log(flag);
+    //console.log(flag);
     let oneIsChecked = false;
     for (box of boxes) {
         //console.log(box);
         if (box.checked) {
             oneIsChecked = true;
+            // better to have valid on the checkbox than on the flag
+            box.classList.add('valid'); 
+        }
+        else {
+            box.classList.remove('valid');
         }
     }
-    if (oneIsChecked){
-        flag.classList.add('valid');
-    }
-    else {
-        flag.classList.remove('valid');
-    }
+    let flag = parentElt.querySelector('.flag');
     console.log(oneIsChecked);
     return oneIsChecked;   
 }
